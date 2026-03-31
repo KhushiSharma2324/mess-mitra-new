53a86b7f07d54a8b0b26ec92a43df4c5c95822f4
khushi sharma <khushisharma9124@gmail.com>
2026-04-01 00:00:17 +0530

    chatbot added successfully

--- backend/test-models.js
+++
@@ -1,7 +0,0 @@
-fetch("https://generativelanguage.googleapis.com/v1beta/models?key=AIzaSyATg15_gEMcB-tdepRZIxWPCBblX6X9HAE").then(r=>r.json()).then(data => {
-  if (data.models) {
-    console.log(data.models.map(m => m.name).join('\n'));
-  } else {
-    console.log("No models returned:", data);
-  }
-}).catch(console.error);
